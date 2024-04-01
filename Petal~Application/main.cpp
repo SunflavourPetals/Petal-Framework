4593e271e3f836ccdb4813804fc76d2a634043e8
SunflavourPetals <105978614+SunflavourPetals@users.noreply.github.com>
2024-04-01 23:56:37 +0800

    测试 rawinput window

--- Petal~Application/main.cpp
+++ Petal~Application/main.cpp
@@ -1,18 +1,56 @@
-#include "Petal~Window.h"
+#include "Petal~RawInputWindow.h"
 #include "Petal~VSDebugOutput.h"
 
 namespace App
 {
-	class AppWindow : public Petal::Window // comment
+	class AppWindow : public Petal::RawInputWindow
 	{
 	public:
-		AppWindow() : Window()  /* comment */
+		void RawKeyboardEvent(
+			Petal::RawKeyboardMessage& e,
+			Petal::Win32RawInput& raw_input,
+			Petal::tsize raw_input_size) noexcept
+			override
+		{
+			Petal::Debug::println("KEYBOARD");
+		}
+		void RawHidEvent(
+			Petal::RawHidMessage& e,
+			Petal::Win32RawInput& raw_input,
+			Petal::tsize raw_input_size) noexcept
+			override
+		{
+			Petal::Debug::println("HID");
+		}
+		AppWindow() : RawInputWindow()
 		{
 			window_class.Register();
 			Create(window_class.ClassAtom());
 			UpdateTitle(Petal_TStr("Hello Visual Studio 2022 Community Preview"));
 			Show();
 			UpdateWindow();
+			Petal::Win32RawInputDevice rid[]
+			{
+				{
+					.usUsagePage = HID_USAGE_PAGE_GENERIC,
+					.usUsage = HID_USAGE_GENERIC_KEYBOARD,
+					.dwFlags = RIDEV_NOLEGACY,
+					.hwndTarget = nullptr,
+				},
+				{
+					.usUsagePage = HID_USAGE_PAGE_GENERIC,
+					.usUsage = HID_USAGE_GENERIC_GAMEPAD,
+					.dwFlags = 0,
+					.hwndTarget = nullptr,
+				},
+				{
+					.usUsagePage = HID_USAGE_PAGE_GENERIC,
+					.usUsage = HID_USAGE_GENERIC_JOYSTICK,
+					.dwFlags = 0,
+					.hwndTarget = nullptr,
+				}
+			};
+			RegisterRawInputDevices(rid);
 			Petal::Debug::println("Hello Visual Studio 2022 Community Preview");
 		}
 		~AppWindow()
