9a59f6c6af54f7befea93a1d5592ccf132360cda
SunflavourPetals <105978614+SunflavourPetals@users.noreply.github.com>
2024-04-04 03:07:57 +0800

    改 Rawinputkeyboardcontroller 为 headonly

--- Petal~Application/Petal~RawInputKeyboardController.h
+++ Petal~Application/Petal~RawInputKeyboardController.h
@@ -10,9 +10,13 @@ namespace Petal::Keyboard
 	class RawInputController final : public Abstract::Keyboard::Controller
 	{
 	protected:
-		void QueryState() noexcept override;
+		void QueryState() noexcept override {};
 	public:
-		void UpdateKeyState(VirtualKey::Type vk_code, boolean pushed) noexcept;
+		void UpdateKeyState(VirtualKey::Type vk_code, boolean pushed) noexcept
+		{
+			this->last_state.Set(vk_code, this->state.Pushed(vk_code));
+			this->state.Set(vk_code, pushed);
+		}
 	public:
 		RawInputController() = default;
 		~RawInputController() = default;
